fd629e7ee12ad7346999537be9a3373481a2b098
myusuf4892 <myusup4892@gmail.com>
2022-08-22 10:13:56 +0700

    try assets with 000webhost

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -47,12 +47,12 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="{{ asset('assets/blog/vendor/purecounter/purecounter_vanilla.js') }}"></script>
-  <script src="https://bookstories.000webhostapp.com/blog/vendor/aos/aos.js"></script>
+  <script src="https://bookstories.000webhostapp.com/blog/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="{{ asset('assets/blog/vendor/aos/aos.js') }}"></script>
   <script src="{{ asset('assets/blog/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <script src="{{ asset('assets/blog/vendor/glightbox/js/glightbox.min.js') }}"></script>
   <script src="{{ asset('assets/blog/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
-  <script src="https://bookstories.000webhostapp.com/blog/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="{{ asset('assets/blog/vendor/swiper/swiper-bundle.min.js') }}"></script>
   <script src="{{ asset('assets/blog/vendor/php-email-form/validate.js') }}"></script>
 
   <!-- Template Main JS File -->
